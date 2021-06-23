c7835902a226c2459779dcc2c661bb085e051e98
Farhan <farhan.fs360@gmail.com>
2021-06-23 16:06:37 +0500

    Responsive Done

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,16 +1,51 @@
 $(document).ready(function() {
 
     $('.nurse-slide').slick({
-        //centerMode: true,
-        //centerPadding: '60px',
         slidesToShow: 3,
         arrows: false,
         dots: true,
         autoplay: true,
         //prevArrow: $('.prev'),
         //nextArrow: $('.next'),
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    arrows: false,
+                    centerMode: false,
+                    centerPadding: '40px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: false,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
 
+    $('.vdo-pdf-slider').slick({
+        slidesToShow: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+        fade: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
         responsive: [{
                 breakpoint: 1200,
                 settings: {
@@ -55,12 +90,12 @@ $(document).ready(function() {
 
         if (scroll >= 30) {
             $(".header.header").addClass("darkHeader");
-            $("a.site-logo img").css("max-width", "100px");
+            $("a.site-logo").addClass("site-logo2");
             $("header.header ul.nav").css("gap", "20px");
 
         } else {
             $(".header.header").removeClass("darkHeader");
-            $("a.site-logo img").css("max-width", "155px");
+            $("a.site-logo").removeClass("site-logo2");
             $("header.header ul.nav").css("gap", "30px");
         }
     });
